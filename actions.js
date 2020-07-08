2537e0a62160550089dab24ae4f6cb7ddfe53665
Shruti Pahadiya <15.shruti@gmail.com>
2020-07-08 18:56:10 -0400

    redux stretch2

--- actions.js
+++ actions.js
@@ -4,4 +4,20 @@ const axios = require('axios');
 const { GOT_DESSERTS_DATA, NEW_DESSERT_DATA } = require('./types');
 
 /* YOUR CODE GOES HERE */
+const got_desserts = ({desserts}) => ({
+
+    type:GOT_DESSERTS_DATA,
+    desserts,
+});
+
+const new_dessert = ({newDessert}) => ({
+
+    type:NEW_DESSERT_DATA,
+    newDessert,
+});
+
+const mapDispatchToProps =(dispatch,newDessert)
+
+
+
 module.exports = { gotDesserts, addDessert, fetchDesserts, postDessert };
